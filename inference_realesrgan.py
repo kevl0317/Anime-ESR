eabedd95c6d47bb2637a7d167cac0eec9febd6a0
Kevin Liu <kevin.liu1270@gmail.com>
2025-05-21 13:35:27 +0800

    removed and modified redundant files

--- inference_realesrgan.py
+++ inference_realesrgan.py
@@ -83,22 +83,16 @@ def main():
             'https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.5.0/realesr-general-wdn-x4v3.pth',
             'https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.5.0/realesr-general-x4v3.pth'
         ]
-    elif args.model_name == 'custom_model':
-        model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=4)
-        netscale = 4
-        file_url = None  # No download needed
-
 
     # determine model paths
     if args.model_path is not None:
         model_path = args.model_path
     else:
-        if args.model_name == 'custom_model':
-            raise ValueError('Please specify --model_path for custom_model')
         model_path = os.path.join('weights', args.model_name + '.pth')
         if not os.path.isfile(model_path):
             ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
             for url in file_url:
+                # model_path will be updated
                 model_path = load_file_from_url(
                     url=url, model_dir=os.path.join(ROOT_DIR, 'weights'), progress=True, file_name=None)
 
